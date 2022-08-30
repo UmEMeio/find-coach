8400b7c53f56d14617523e1c6c33c0308b8def53
Gabriel Costa <52284050+UmEMeio@users.noreply.github.com>
2022-08-30 10:10:11 -0300

    Adicionando rotas e preparando o ambiente

---
+++ src/routes.js
@@ -0,0 +1,10 @@
+import { createRouter, createWebHistory } from "vue-router";
+import coachPage from './pages/coachPage.vue';
+import requestPage from './pages/requestPage.vue'
+const routes = [{ path: '/', redirect: coachPage }, { path: '/coach', component: coachPage }, { path: '/requests', component: requestPage }]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+export default router

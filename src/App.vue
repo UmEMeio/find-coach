7d8520b4e16600a7af7e11a09481b71280e7b115
Gabriel Costa <52284050+UmEMeio@users.noreply.github.com>
2022-09-05 10:48:13 -0300

    Adicionando BaseComponents, e ajustando o Header

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,23 @@
 <template>
+  <the-header />
   <router-view></router-view>
 </template>
+<script>
+import TheHeader from "./components/layouts/TheHeader.vue";
+export default {
+  components: { TheHeader },
+};
+</script>
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,9 @@
 import { createRouter, createWebHistory } from "vue-router";
 import coachPage from './pages/coachPage.vue';
 import requestPage from './pages/requestPage.vue'
-const routes = [{ path: '/', redirect: coachPage }, { path: '/coach', component: coachPage }, { path: '/requests', component: requestPage }]
+
+
+const routes = [{ path: '/', redirect: '/coach' }, { path: '/coach', component: coachPage }, { path: '/requests', component: requestPage }]
 
 const router = createRouter({
   history: createWebHistory(),
